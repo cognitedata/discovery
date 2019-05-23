b82efc176e88c001d59562b38d30c412612fd4f3
Anders Hafreager <anders.hafreager@cognite.com>
2019-05-23 14:47:05 +0200

    Added simple 3D viewer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'antd/dist/antd.css';
+import { ReactAuthProvider } from '@cognite/react-auth';
+
+import {
+  Cognite3DViewer,
+  Cognite3DModel,
+  OnProgressData,
+} from '@cognite/3d-viewer';
+import * as sdk from '@cognite/sdk';
+import { Model3DViewer } from '@cognite/gearbox';
+
+function ThreeDViewer(props) {
+  const cache = {};
+  const modelId = 2495544803289093;
+  const revisionId = 3041181389296996;
+  const onClick = (nodeId) => {};
+  const onProgress = (progress) => {};
+  const onComplete = () => {};
+  const onReady = (
+    viewer,
+    model,
+    revision
+  ) => {};
+  
+  return (
+    <Model3DViewer
+      modelId={modelId}
+      revisionId={revisionId}
+      onClick={onClick}
+      onProgress={onProgress}
+      onComplete={onComplete}
+      onReady={onReady}
+      cache={cache}
+    />
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ReactAuthProvider
+    project={'akerbp'}
+    redirectUrl={window.location.href}
+    errorRedirectUrl={window.location.href}
+  >
+    <ThreeDViewer />
+  </ReactAuthProvider>
   );
 }
 
