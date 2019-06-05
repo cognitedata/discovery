32d98acb73d00216633526fc8e5cb9ff58ce992a
Anders Hafreager <anders.hafreager@cognite.com>
2019-06-05 16:16:44 +0200

    Added asset module

---
+++ src/modules/assets.js
@@ -0,0 +1,62 @@
+import { createAction } from 'redux-actions';
+import PropTypes from 'prop-types';
+import * as sdk from '@cognite/sdk';
+
+// Constants
+export const SET_ASSETS = 'assets/SET_ASSETS';
+
+export const Assets = PropTypes.exact({
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      name: PropTypes.string,
+      description: PropTypes.string,
+    })
+  ),
+});
+
+let lastRequest = 0;
+// Functions
+export function searchForAsset(query, name) {
+  return async dispatch => {
+    const now = Date.now();
+    lastRequest = now;
+
+    const result = await sdk.Assets.search({ query, name, limit: 100 });
+    const assets_ = result.items.map(asset => ({
+      id: asset.id,
+      name: asset.name,
+      description: asset.description,
+    }));
+    if (now === lastRequest) {
+      dispatch({ type: SET_ASSETS, payload: { items: assets_ } });
+    }
+  };
+}
+
+// Reducer
+const initialState = {};
+
+export default function assets(state = initialState, action) {
+  switch (action.type) {
+    case SET_ASSETS: {
+      const { items } = action.payload;
+      return {
+        ...state,
+        items,
+      };
+    }
+    default:
+      return state;
+  }
+}
+
+// Action creators
+const setAssets = createAction(SET_ASSETS);
+
+export const actions = {
+  setAssets,
+};
+
+// Selectors
+export const selectAssets = state => state.assets || { items: [] };
