9eb4e3703e9a093c9f1977ec4405619200006ab3
Anders Hafreager <anders.hafreager@cognite.com>
2019-06-10 23:00:34 +0200

    Only redirect to asset if akerbp

--- src/containers/Auth.js
+++ src/containers/Auth.js
@@ -3,27 +3,33 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { ReactAuthProvider } from '@cognite/react-auth';
 import { Route, Redirect, Switch } from 'react-router-dom';
+import * as sdk from '@cognite/sdk';
 import Main from './Main';
 
-const Auth = ({ tenant, match }) => (
-  <ReactAuthProvider
-    project={tenant}
-    redirectUrl={window.location.href}
-    errorRedirectUrl={window.location.href}
-    usePopup
-    enableTokenCaching
-  >
-    <Switch>
-      <Redirect
-        exact
-        strict
-        from={`${match.url}`}
-        to={`${match.url}/asset/7446334693628062`}
-      />
-      <Route component={Main} />
-    </Switch>
-  </ReactAuthProvider>
-);
+const Auth = ({ tenant, match }) => {
+  const { project } = sdk.configure({});
+  return (
+    <ReactAuthProvider
+      project={tenant}
+      redirectUrl={window.location.href}
+      errorRedirectUrl={window.location.href}
+      usePopup
+      enableTokenCaching
+    >
+      <Switch>
+        {project === 'akerbp' && (
+          <Redirect
+            exact
+            strict
+            from={`${match.url}`}
+            to={`${match.url}/asset/7446334693628062`}
+          />
+        )}
+        <Route component={Main} />
+      </Switch>
+    </ReactAuthProvider>
+  );
+};
 Auth.propTypes = {
   tenant: PropTypes.string.isRequired,
   match: PropTypes.shape({
