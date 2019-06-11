71dbc7ad573f7b35300f6195ebf2ad6675e5a6c6
Anders Hafreager <anders.hafreager@cognite.com>
2019-06-11 15:28:41 +0200

    Render details on workorder events

--- src/components/EventPreview.js
+++ src/components/EventPreview.js
@@ -1,10 +1,45 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Modal } from 'antd';
+import { Modal, List } from 'antd';
 
 import { EventPreview as GearboxEventPreview } from '@cognite/gearbox';
 
 class EventPreview extends React.PureComponent {
+  state = {
+    showDetails: undefined,
+  };
+
+  renderDetailsWorkorder() {
+    const event = this.state.showDetails;
+    const fields = {
+      DESCRIPTION_1: 'Description',
+      LOCATION: 'Area',
+      OBJECTNUMBER: 'Object number',
+      SOURCE: 'Source',
+      WORKORDER_ID: 'Work order id',
+    };
+
+    return (
+      <List
+        dataSource={Object.keys(fields)}
+        renderItem={item => (
+          <List.Item.Meta
+            title={fields[item]}
+            description={event.metadata[item]}
+          />
+        )}
+      />
+    );
+  }
+
+  renderDetails() {
+    const event = this.state.showDetails;
+    if (event.type === 'Workorder') {
+      return this.renderDetailsWorkorder();
+    }
+    return <></>;
+  }
+
   render() {
     return (
       <Modal
@@ -13,7 +48,13 @@ class EventPreview extends React.PureComponent {
         onCancel={this.props.onClose}
         footer={[null, null]}
       >
-        <GearboxEventPreview eventId={this.props.eventId} />
+        <GearboxEventPreview
+          eventId={this.props.eventId}
+          onShowDetails={event => {
+            this.setState({ showDetails: event });
+          }}
+        />
+        {this.state.showDetails && this.renderDetails()}
       </Modal>
     );
   }
