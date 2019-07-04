30376fa62689b99b45f90045f735d8c163fc2d70
Anders Hafreager <anders.hafreager@cognite.com>
2019-07-04 19:44:39 +0200

    Improved camera animation

--- src/components/Model3D.js
+++ src/components/Model3D.js
@@ -2,10 +2,85 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Model3DViewer } from '@cognite/gearbox';
-import { THREE } from '@cognite/3d-viewer';
-import { Slider } from 'antd';
+import { THREE, TWEEN } from '@cognite/3d-viewer';
 import LoadingScreen from './LoadingScreen';
 import { selectResult } from '../modules/search';
+
+function clamp(val, min, max) {
+  return Math.max(min, Math.min(val, max));
+}
+
+function animateCamera(position, target, viewer, duration = 1000) {
+  const { _camera: camera } = viewer;
+
+  if (duration == null) {
+    const distance = position.distanceTo(camera.position);
+    duration = distance * 125; // 250ms per unit distance
+    duration = clamp(duration, 600, 2500); // min duration 600ms and 2500ms as max duration
+  }
+
+  const raycaster = new THREE.Raycaster();
+  raycaster.setFromCamera(new THREE.Vector2(0, 0), camera);
+  const distanceToTarget = target.distanceTo(camera.position);
+  const scaledDirection = raycaster.ray.direction
+    .clone()
+    .multiplyScalar(distanceToTarget);
+  const startTarget = raycaster.ray.origin.clone().add(scaledDirection);
+
+  const from = {
+    x: camera.position.x,
+    y: camera.position.y,
+    z: camera.position.z,
+    targetX: startTarget.x,
+    targetY: startTarget.y,
+    targetZ: startTarget.z,
+  };
+  const to = {
+    x: position.x,
+    y: position.y,
+    z: position.z,
+    targetX: target.x,
+    targetY: target.y,
+    targetZ: target.z,
+  };
+
+  const tween = new TWEEN.Tween(from);
+
+  const tmpTarget = new THREE.Vector3();
+  const tmpPosition = new THREE.Vector3();
+  tween
+    .to(to, duration)
+    .easing(TWEEN.Easing.Circular.Out)
+    .onUpdate(() => {
+      tmpPosition.set(from.x, from.y, from.z);
+      tmpTarget.set(from.targetX, from.targetY, from.targetZ);
+
+      viewer.setCameraPosition(tmpPosition);
+      viewer.setCameraTarget(tmpTarget);
+    })
+    .start();
+}
+
+function fitCameraToBoundingBox(box, viewer, duration) {
+  const boundingSphere = new THREE.Sphere();
+  box.getBoundingSphere(boundingSphere);
+  const target = boundingSphere.center;
+
+  const distance = Math.max(
+    2.0 * viewer._getSmallestMeanObjectSize(), // this is the smallest value controls.minDistance can have
+    boundingSphere.radius * 4
+  ); // distance from target
+
+  const direction = new THREE.Vector3(0, 1, 0.7);
+  direction.normalize();
+
+  const position = new THREE.Vector3();
+  position
+    .copy(direction)
+    .multiplyScalar(distance)
+    .add(target);
+  animateCamera(position, target, viewer, duration);
+}
 
 class Model3D extends React.Component {
   state = {};
@@ -16,7 +91,7 @@ class Model3D extends React.Component {
 
   componentDidMount() {}
 
-  componentDidUpdate(prevProps, prevState) {
+  componentDidUpdate(prevProps) {
     if (prevProps.result !== this.props.result && this.state.model) {
       this.updateSearchResult();
     }
@@ -43,21 +118,56 @@ class Model3D extends React.Component {
       model,
       viewer,
     });
+
     window.viewer = viewer;
     window.THREE = THREE;
     window.model = model;
   };
 
-  selectNode = (mapping, animate, duration = 500) => {
-    const boundingBox = new THREE.Box3();
+  selectNode = mapping => {
     this.iterateNodeSubtree(mapping.treeIndex, mapping.subtreeSize, nodeId => {
       this.state.model.selectNode(nodeId);
-      boundingBox.union(this.state.model.getBoundingBox(nodeId));
     });
+  };
 
-    if (animate) {
-      this.state.viewer.fitCameraToBoundingBox(boundingBox, duration);
+  fitCameraToNodes = mappings => {
+    if (mappings.length === 0) {
+      return;
     }
+
+    const { viewer } = this.state;
+
+    const boundingBox = new THREE.Box3();
+    mappings.forEach(mapping => {
+      this.iterateNodeSubtree(
+        mapping.treeIndex,
+        mapping.subtreeSize,
+        nodeId => {
+          boundingBox.union(this.state.model.getBoundingBox(nodeId));
+        }
+      );
+    });
+    if (mappings.length > 1) {
+      // We want to view it from above
+      const center = new THREE.Vector3();
+      boundingBox.getCenter(center);
+      const target = center.clone();
+
+      center.z += 3;
+      center.y += 10;
+      const position = center.clone();
+
+      animateCamera(position, target, viewer);
+    } else {
+      fitCameraToBoundingBox(boundingBox, viewer);
+    }
+
+    // Set slicing to show the result
+    const plane = new THREE.Plane(
+      new THREE.Vector3(0, -1, 0),
+      boundingBox.max.y + 0.5
+    );
+    viewer.setSlicingPlanes([plane]);
   };
 
   iterateNodeSubtree = (treeIndex, subtreeSize, action) => {
@@ -69,25 +179,6 @@ class Model3D extends React.Component {
     }
   };
 
-  setSlicingForCurrentAsset = () => {
-    const boundingBox = new THREE.Box3();
-    this.iterateNodeSubtree(this.props.nodeId, id => {
-      boundingBox.union(this.state.model.getBoundingBox(id));
-    });
-
-    const plane = new THREE.Plane(
-      new THREE.Vector3(0, -1, 0),
-      boundingBox.max.y + 0.5
-    );
-    this.viewer.setSlicingPlanes([plane]);
-  };
-
-  onSliderChange = change => {
-    const plane1 = new THREE.Plane(new THREE.Vector3(0, -1, 0), change[1]);
-    const plane2 = new THREE.Plane(new THREE.Vector3(0, 1, 0), -change[0]);
-    this.viewer.setSlicingPlanes([plane1, plane2]);
-  };
-
   updateSearchResult() {
     const threed = this.props.result.filter(result => result.kind === '3d');
 
@@ -95,13 +186,11 @@ class Model3D extends React.Component {
       this.state.model.deselectAllNodes();
 
       this.colorNodes(threed[0].mappings);
+      this.fitCameraToNodes(threed[0].mappings);
       if (threed[0].mappings.length === 1) {
         this.selectNode(threed[0].mappings[0], true);
       }
     }
-    // if (this.model) {
-    //   this.selectNode(this.props.nodeId, true, 1500);
-    // }
   }
 
   colorNodes(mappings) {
@@ -141,21 +230,6 @@ class Model3D extends React.Component {
           onProgress={this.onProgress}
           onComplete={this.onComplete}
           cache={this.props.cache}
-        />
-        <Slider
-          vertical
-          step={0.1}
-          defaultValue={[495, 510]}
-          range
-          min={495}
-          max={510}
-          style={{
-            position: 'absolute',
-            paddingLeft: 10,
-            paddingTop: 10,
-            height: '50%',
-          }}
-          onChange={this.onSliderChange}
         />
       </>
     );
