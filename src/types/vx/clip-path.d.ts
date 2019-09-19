64ad6027cc99bc07067a2dfe8e22e23660d88a0f
David Liu <david.liu@cognite.com>
2019-09-19 11:11:13 +0200

    add types

---
+++ src/types/vx/clip-path.d.ts
@@ -0,0 +1,33 @@
+// Type definitions for @vx/clip-path 0.0.183
+// Project: https://github.com/hshoff/vx
+// Definitions by: Giacomo Debidda <https://github.com/jackdbd>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare module '@vx/clip-path' {
+  import React, { SVGAttributes } from 'react';
+
+  interface IProps {
+    id: string;
+    children: React.ReactNode;
+  }
+
+  interface ICircleProps extends SVGAttributes<SVGCircleElement> {
+    id: string;
+    cx?: number | string;
+    cy?: number | string;
+    r?: number | string;
+  }
+
+  interface IRectProps extends SVGAttributes<SVGRectElement> {
+    height?: number | string;
+    id: string;
+    width?: number | string;
+    x?: number | string;
+    y?: number | string;
+  }
+
+  const ClipPath: React.ComponentType<IProps>;
+
+  const CircleClipPath: React.ComponentType<ICircleProps>;
+  const RectClipPath: React.ComponentType<IRectProps>;
+}
