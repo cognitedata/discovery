3d290ddab6aef81a47271344c62a4c7fca675f5d
David Liu <35227776+dlcognite@users.noreply.github.com>
2019-09-20 13:52:53 +0200

    Add fix to search on description (#40)

--- src/containers/MapModelToAssetForm.tsx
+++ src/containers/MapModelToAssetForm.tsx
@@ -61,7 +61,7 @@ class MapModelToAssetForm extends React.Component<Props, State> {
     if (query.length > 0) {
       this.setState({ fetching: true });
       const results = await sdk.assets.search({
-        search: { name: query },
+        search: { name: query, description: query },
         filter: {
           root: true,
         },
