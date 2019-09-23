dc5b9655c5e076af9e235964c5e21e58318a78f7
David Liu <david.liu@cognite.com>
2019-09-23 14:03:56 +0200

    Updated with working PNID linking

--- src/containers/PNIDViewer.tsx
+++ src/containers/PNIDViewer.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { List, Button, message } from 'antd';
+import { List, Button, message, Icon } from 'antd';
 import styled from 'styled-components';
 import { SVGViewer } from '@cognite/gearbox';
 import { bindActionCreators, Dispatch } from 'redux';
@@ -17,14 +17,16 @@ const getTextFromMetadataNode = (node: { textContent?: string }) =>
 const ViewerContainer = styled.div`
   height: 100%;
   width: 100%;
-  padding: 24px;
-  padding-top: 75px;
-  background: #f5f5f5;
+  padding: 12px;
+  display: flex;
+  flex-direction: column;
 `;
 
 const StyledSVGViewerContainer = styled.div`
-  height: 100%;
+  flex: 1;
+  max-height: 100%;
   width: 100%;
+  position: relative;
   .myCoolThing {
     outline: auto 2px #3838ff;
     transition: all 0.2s ease;
@@ -55,15 +57,17 @@ type Props = {
 };
 
 type State = {
-  currentFile?: { id: number; fileName: string };
+  currentFiles: { id: number; fileName: string }[];
+  currentIndex: number;
 };
 
 class PNIDViewer extends React.Component<Props, State> {
-  readonly state: Readonly<State> = {
-    currentFile: undefined,
-  };
-
   svgviewer?: SVGViewer | null = undefined;
+
+  readonly state: Readonly<State> = {
+    currentFiles: [],
+    currentIndex: 0,
+  };
 
   componentDidUpdate(prevProps: Props) {
     if (prevProps.app.assetId !== this.props.app.assetId) {
@@ -82,6 +86,14 @@ class PNIDViewer extends React.Component<Props, State> {
         }
       }, 100);
     }
+  }
+
+  get currentFile() {
+    const { currentFiles, currentIndex } = this.state;
+    if (currentFiles.length === 0) {
+      return undefined;
+    }
+    return currentFiles[currentIndex];
   }
 
   searchAndSelectAssetName = async (nameString: string) => {
@@ -117,7 +129,7 @@ class PNIDViewer extends React.Component<Props, State> {
   };
 
   loadNextPNID = async (nameString: string) => {
-    let { currentFile } = this.state;
+    const { currentFiles } = this.state;
 
     let name = nameString;
 
@@ -131,15 +143,15 @@ class PNIDViewer extends React.Component<Props, State> {
     const result = await sdk.files.search({ search: { name: `${name}.svg` } });
     const exactMatch = result.find(a => a.name === `${name}.svg`);
     if (exactMatch) {
-      currentFile = {
+      currentFiles.push({
         id: exactMatch.id,
         fileName: exactMatch.name,
-      };
+      });
     } else {
       message.info('Did not find next pnid graph');
       return;
     }
-    this.setState({ currentFile });
+    this.setState({ currentFiles, currentIndex: currentFiles.length - 1 });
   };
 
   isCurrentAsset = (metadata: any) => {
@@ -156,37 +168,58 @@ class PNIDViewer extends React.Component<Props, State> {
   };
 
   renderSVGViewer() {
+    const { currentFiles, currentIndex } = this.state;
     return (
-      <StyledSVGViewerContainer>
-        <SVGViewer
-          ref={c => {
-            this.svgviewer = c; // Will direct access this
-          }}
-          documentId={this.state.currentFile ? this.state.currentFile.id : 0}
-          title={this.state.currentFile && this.state.currentFile.fileName}
-          description="P&ID"
-          handleCancel={() => {
-            this.setState({ currentFile: undefined });
-          }}
-          isCurrentAsset={this.isCurrentAsset}
-          // metadataClassesConditions={[
-          //   {
-          //     condition: node => {
-          //       return getTextFromMetadataNode(node) === '13PST1233';
-          //     },
-          //     className: 'myCoolThing',
-          //   },
-          // ]}
-          handleItemClick={item => {
-            const name = item.children[0].children[0].innerHTML;
-            if (item.children[0].children[0].tagName === 'file_id') {
-              this.loadNextPNID(name);
-            } else {
-              this.searchAndSelectAssetName(name);
-            }
-          }}
-        />
-      </StyledSVGViewerContainer>
+      <>
+        <Button.Group style={{ marginBottom: '12px' }}>
+          <Button
+            size="small"
+            disabled={currentIndex === 0}
+            onClick={() => this.setState({ currentIndex: currentIndex - 1 })}
+          >
+            <Icon type="left" />
+            Previous Document
+          </Button>
+          <Button
+            size="small"
+            disabled={currentIndex === currentFiles.length - 1}
+            onClick={() => this.setState({ currentIndex: currentIndex + 1 })}
+          >
+            Next Document
+            <Icon type="right" />
+          </Button>
+        </Button.Group>
+        <StyledSVGViewerContainer>
+          <SVGViewer
+            ref={c => {
+              this.svgviewer = c; // Will direct access this
+            }}
+            documentId={this.currentFile ? this.currentFile.id : 0}
+            title={this.currentFile && this.currentFile.fileName}
+            description="P&ID"
+            handleCancel={() => {
+              this.setState({ currentFiles: [], currentIndex: 0 });
+            }}
+            isCurrentAsset={this.isCurrentAsset}
+            // metadataClassesConditions={[
+            //   {
+            //     condition: node => {
+            //       return getTextFromMetadataNode(node) === '13PST1233';
+            //     },
+            //     className: 'myCoolThing',
+            //   },
+            // ]}
+            handleItemClick={item => {
+              const name = item.children[0].children[0].innerHTML;
+              if (item.children[0].children[0].tagName === 'file_id') {
+                this.loadNextPNID(name);
+              } else {
+                this.searchAndSelectAssetName(name);
+              }
+            }}
+          />
+        </StyledSVGViewerContainer>
+      </>
     );
   }
 
@@ -217,7 +250,8 @@ class PNIDViewer extends React.Component<Props, State> {
               type="link"
               onClick={() => {
                 this.setState({
-                  currentFile: { id: item.id, fileName: item.name },
+                  currentFiles: [{ id: item.id, fileName: item.name }],
+                  currentIndex: 0,
                 });
               }}
             >
@@ -230,7 +264,7 @@ class PNIDViewer extends React.Component<Props, State> {
   }
 
   render() {
-    const { currentFile } = this.state;
+    const { currentFile } = this;
 
     return (
       <ViewerContainer>
