05111d7566c86aa3a113809be3e25b05abae4936
David Liu <david.liu@cognite.com>
2019-10-15 18:23:14 +0200

    fix clicking outside

--- src/containers/FileExplorer/FileUploadTab.tsx
+++ src/containers/FileExplorer/FileUploadTab.tsx
@@ -205,12 +205,20 @@ class FileUploadTab extends React.Component<Props, State> {
                   style={{ width: '100%' }}
                   placeholder="Search for an asset to link to"
                   value={includeAssetId}
+                  onDropdownVisibleChange={console.log}
                   notFoundContent={fetching ? <Spin size="small" /> : null}
-                  onChange={(id: any) =>
+                  onSelect={(id: any) =>
                     this.setState({
                       includeAssetId: id ? Number(id) : undefined,
                     })
                   }
+                  onChange={(id: any) => {
+                    if (!id) {
+                      this.setState({
+                        includeAssetId: undefined,
+                      });
+                    }
+                  }}
                   onSearch={this.doSearch}
                   filterOption={false}
                   allowClear
